5a666b7646b928dd6289a0361fd3a8714c7cdadc
XIAOMi <check.point9917@gmail.com>
2020-05-20 21:31:29 +0300

    факториал

--- while.py
+++ while.py
@@ -1,6 +1,16 @@
 i = 0
+result = 1000
 while True:
-    if i > 10:
+    if i >result:
         break
     print(i)
     i += 1
+
+
+x = int(input('Введите число: '))
+fact = 1
+while x > 1:
+    fact *= x
+    x -= 1
+
+print(fact)
