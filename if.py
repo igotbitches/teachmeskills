f6d9f720bf31bf1ee1ea321d0de7715d1522e7a9
XIAOMi <check.point9917@gmail.com>
2020-05-20 21:04:12 +0300

    abc

--- if.py
+++ if.py
@@ -2,5 +2,5 @@ a = int(input("Введите число a: "))
 b = int(input("Введите число b: "))
 c = int(input("Введите число c: "))
 
-if a>20 and b<20 or c==20:
+if a > 20 and b < 20 or c == 20:
     print(True)
