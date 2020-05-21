eefe1f441865b1616bdb2a7a9d78e5367505ca51
XIAOMi <check.point9917@gmail.com>
2020-05-21 10:37:38 +0300

    Факториал целого числа с использованием "for" и "while"

--- day1/factorial.py
+++ day1/factorial.py
@@ -1,7 +1,18 @@
-x = int(input('Введите число: '))
+#first option using "for"
+
+x = int(input('Введите целое число: '))
+factorial = 1
+for i in range(x):
+    factorial *= (i+1)
+print(factorial)
+
+
+#second option using "while"
+
+x = int(input('Введите целое число: '))
 fact = 1
 while x > 1:
-    fact *= x
+    fact = fact * x
     x -= 1
-
 print(fact)
+
