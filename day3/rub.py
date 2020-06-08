f5d3967f980f2c6229cbb5c1d0cf1ed708aa082c
Nick <check.point9917@yandex.ru>
2020-06-08 18:17:16 +0300

    Рубли, копейки. Теперь все слова выводит корректно (но всё-таки мог что-то не учесть). Так же поправил вывод. Теперь, если ввести только копейки или только рубли, на жкран выведутся только введенные значения.

--- day3/rub.py
+++ day3/rub.py
@@ -6,11 +6,11 @@ cent = total % 100
 rub_word = ''
 cent_word = ''
 
-if rub == 1 or rub % 10 == 1 and rub%100 != 11:
+if rub == 1 or rub % 10 == 1 and rub % 100 != 11:
     rub_word = 'рубль'
-elif rub%100 == 11 or rub%100 == 12 or rub%100 == 13 or rub%100 == 14:
+elif rub % 100 == 11 or rub % 100 == 12 or rub % 100 == 13 or rub % 100 == 14:
     rub_word = 'рублей'
-elif rub%10 == 2 or rub%10 == 3 or rub%10 == 4:
+elif rub % 10 == 2 or rub % 10 == 3 or rub % 10 == 4:
     rub_word = 'рубля'
 else:
     rub_word = 'рублей'
@@ -19,7 +19,7 @@ if cent == 1 or cent % 10 == 1 and cent != 11:
     cent_word = 'копейка'
 elif cent == 11 or cent == 12 or cent == 13 or cent == 14:
     cent_word = 'копеек'
-elif cent%10 == 2 or cent%10 == 3 or cent%10 == 4:
+elif cent % 10 == 2 or cent % 10 == 3 or cent % 10 == 4:
     cent_word = 'копейки'
 else:
     cent_word = 'копеек'
