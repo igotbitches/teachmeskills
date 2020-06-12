c5836413e39edcef899f3e9a99daca9d34a932d0
Nick <check.point9917@yandex.ru>
2020-06-12 21:04:06 +0300

    Средний балл

--- day5/groups.py
+++ day5/groups.py
@@ -29,7 +29,8 @@ pupils = [
   }
 ]
 
-def average():
-    for i in pupils:
-        average = round(((i['physics'] + i['informatics'] + i['history']) / 3), 2)
-        print(average)
+for i in pupils:
+    average = round(((i['physics'] + i['informatics'] + i['history']) / 3), 2)
+    if average > 4:
+        print(f"Имя: {i['firstname']}, Группа: {i['Group']}, Средний балл: {average}")
+
