878d869c82729f970a5ef73b4cecebc2282578f5
Nick <check.point9917@yandex.ru>
2020-06-12 12:55:18 +0300

    Задача 7. Перевод десятичного числа в системы octal и binary.

--- day4/ex7_binary_octal_systems.py
+++ day4/ex7_binary_octal_systems.py
@@ -1,5 +1,14 @@
 
 def system_2_and_8(number, system):
+    """
+    Перевод десятичного числа в системы octal и binary.
+    :param number: Чисто десятичной системы счисления
+    :type number: int
+    :param system: Система счисления, в которую нужно перевести число десятичной системы
+    :type system: int
+    :return: число octal/binary в виде списка
+    :rtype: list
+    """
     if system == 2:
         number_2 = []
         while number != 0:
